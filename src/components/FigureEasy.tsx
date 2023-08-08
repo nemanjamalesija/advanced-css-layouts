4a5a57053f9cc194097087ecacbd1734fd91274e
nemanjamalesija <malesija13@gmail.com>
2023-08-08 06:35:58 +0200

    Add rest of the content

---
+++ src/components/FigureEasy.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const FigureEasy = () => {
+  return (
+    <figure>
+      <img
+        src='https://assets.codepen.io/296057/fem-blindfold-475.jpg'
+        alt='The three men of Magenta Lime, blindfolded.'
+      />
+      <figcaption>Magenta Lime will rock your world.</figcaption>
+    </figure>
+  );
+};
+
+export default FigureEasy;
--- src/versions/EasyVersion.tsx
+++ src/versions/EasyVersion.tsx
@@ -1,10 +1,14 @@
 import { FC } from 'react';
 import HeaderEasy from '../components/HeaderEasy';
+import MainEasy from '../components/MainEasy';
+import FigureEasy from '../components/FigureEasy';
 
 const EasyVersion: FC = () => {
   return (
     <div>
       <HeaderEasy />
+      <MainEasy />
+      <FigureEasy />
     </div>
   );
 };
