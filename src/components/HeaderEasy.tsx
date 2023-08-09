7b081f035bb75f83f3e5f25305f59e0babb99789
nemanjamalesija <malesija13@gmail.com>
2023-08-09 18:54:57 +0200

    Style header easy

--- src/components/HeaderEasy.tsx
+++ src/components/HeaderEasy.tsx
@@ -2,30 +2,23 @@ import React from 'react';
 
 const HeaderEasy = () => {
   return (
-    <header>
+    <>
+      <header className='header-easy'>
+        <a href='#'>
+          <img
+            className='logo '
+            src='https://assets.codepen.io/296057/fem-maglime-logo.svg'
+            alt='Magenta Lime. Click for home.'
+          />
+        </a>
+      </header>
       <div className='banner'>
         <img
           src='https://assets.codepen.io/296057/fem-blindfold-475.jpg'
           alt='3 men of Magenta Lime.'
         />
       </div>
-      <a href='#'>
-        <img
-          src='https://assets.codepen.io/296057/fem-maglime-logo.svg'
-          alt='Magenta Lime. Click for home.'
-        />
-      </a>
-      <div className='colors'>
-        <div className='lime'></div>
-        <div className='magenta'></div>
-        <div className='magenta50'></div>
-        <div className='white'></div>
-        <div className='tan'></div>
-        <div className='teal'></div>
-        <div className='cyan'></div>
-        <div className='black'></div>
-      </div>
-    </header>
+    </>
   );
 };
 
