cddf9cd39c76545b38545695b68091e9e4c21506
nemanjamalesija <malesija13@gmail.com>
2023-08-11 19:38:43 +0200

    Fix table bug. Refactor to components

---
+++ src/components/HeaderMedium.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const HeaderMedium = () => {
+  return (
+    <header>
+      <a href='#'>
+        <img
+          src='https://assets.codepen.io/296057/fem-maglime-logo.svg'
+          alt='Magenta Lime. Click for home.'
+        />
+      </a>
+    </header>
+  );
+};
+
+export default HeaderMedium;
--- src/versions/MediumVersion.tsx
+++ src/versions/MediumVersion.tsx
@@ -1,101 +1,13 @@
-import React from 'react';
 import AsideMedium from '../components/AsideMedium';
 import FooterMedium from '../components/FooterMedium';
+import MainMedium from '../components/MainMedium';
+import HeaderMedium from '../components/HeaderMedium';
 
 const MediumVersion = () => {
   return (
     <div className='wrapper'>
-      <header>
-        <a href='#'>
-          <img
-            src='https://assets.codepen.io/296057/fem-maglime-logo.svg'
-            alt='Magenta Lime. Click for home.'
-          />
-        </a>
-      </header>
-      <main>
-        <h1 className='h3'>Tour</h1>
-        <p>
-          <span className='date'>31 July 2023</span>
-          <br />
-          <span className='club'>The IVY</span>
-          <br />
-          Oakheart, CA
-        </p>
-        <p>
-          <a className='button-primary button-soldout'>Sold Out</a>
-        </p>
-        <p>
-          <span className='date'>02 August 2023</span>
-          <br />
-          <span className='club'>Club Ecstasy</span>
-          <br />
-          Longdale, CA
-        </p>
-        <p>
-          <a className='button-primary button-soldout'>Sold Out</a>
-        </p>
-        <p>
-          <span className='date'>08 August 2023</span>
-          <br />
-          <span className='club'>The Summit</span>
-          <br />
-          Carran, OR
-        </p>
-        <p>
-          <a href='#' className='button-primary'>
-            Buy Tickets
-          </a>
-        </p>
-        <p>
-          <span className='date'>10 August 2023</span>
-          <br />
-          <span className='club'>Empire</span>
-          <br />
-          Goldenleaf, CA
-        </p>
-        <p>
-          <a href='#' className='button-primary'>
-            Buy Tickets
-          </a>
-        </p>
-        <p>
-          <span className='date'>18 August 2023</span>
-          <br />
-          <span className='club'>The Thunder</span>
-          <br />
-          Stathford, OR
-        </p>
-        <p>
-          <a href='#' className='button-primary'>
-            Buy Tickets
-          </a>
-        </p>
-        <p>
-          <span className='date'>06 September 2023</span>
-          <br />
-          <span className='club'>The Gate</span>
-          <br />
-          Monmouth, WA
-        </p>
-        <p>
-          <a href='#' className='button-primary'>
-            Buy Tickets
-          </a>
-        </p>
-        <p>
-          <span className='date'>11 September 2023</span>
-          <br />
-          <span className='club'>Horizon</span>
-          <br />
-          Colkirk, WA
-        </p>
-        <p>
-          <a href='#' className='button-primary'>
-            Buy Tickets
-          </a>
-        </p>
-      </main>
+      <HeaderMedium />
+      <MainMedium />
       <AsideMedium />
       <FooterMedium />
     </div>
