223087f1d1b59b04023c24f1d408aa1cfff80873
nemanjamalesija <malesija13@gmail.com>
2023-08-12 05:00:23 +0200

    Style medium reviews tablet

--- src/components/ReviewsMedium.tsx
+++ src/components/ReviewsMedium.tsx
@@ -1,146 +1,144 @@
 const ReviewsMedium = () => {
   return (
-    <div>
-      <aside>
-        <figure>
-          <blockquote>
-            Just WOW<br></br>
-            <img
-              src='https://assets.codepen.io/296057/fem-star.svg'
-              alt='Four star rating.'
-              className='star'
-            />
-            <img
-              src='https://assets.codepen.io/296057/fem-star.svg'
-              alt=''
-              className='star'
-            />
-            <img
-              src='https://assets.codepen.io/296057/fem-star.svg'
-              alt=''
-              className='star'
-            />
-            <img
-              src='https://assets.codepen.io/296057/fem-star.svg'
-              alt=''
-              className='star'
-            />
-          </blockquote>
-          <figcaption>
-            <cite>The Staple</cite>
-          </figcaption>
-        </figure>
-        <div className='featured-img'>
-          {' '}
+    <aside className='reviews'>
+      <figure>
+        <blockquote>
+          Just WOW<br></br>
           <img
-            src='https://assets.codepen.io/296057/fem-purple-arms.jpg'
-            alt='Purple stage and excited crowd.'
+            src='https://assets.codepen.io/296057/fem-star.svg'
+            alt='Four star rating.'
+            className='star'
           />
-        </div>
-        <figure>
-          <blockquote>
-            Best. Album. Ever.<br></br>
-            <img
-              src='https://assets.codepen.io/296057/fem-star.svg'
-              alt='Four star rating.'
-              className='star'
-            />
-            <img
-              src='https://assets.codepen.io/296057/fem-star.svg'
-              alt=''
-              className='star'
-            />
-            <img
-              src='https://assets.codepen.io/296057/fem-star.svg'
-              alt=''
-              className='star'
-            />
-            <img
-              src='https://assets.codepen.io/296057/fem-star.svg'
-              alt=''
-              className='star'
-            />
-          </blockquote>
-          <figcaption>
-            <cite>Gold Spin</cite>
-          </figcaption>
-        </figure>
-        <div className='featured-img'>
-          {' '}
           <img
-            src='https://assets.codepen.io/296057/fem-fireworks.jpg'
-            alt='Red stage with fireworks and excited crowd.'
+            src='https://assets.codepen.io/296057/fem-star.svg'
+            alt=''
+            className='star'
           />
-        </div>
-        <figure>
-          <blockquote>
-            These guys know how to party<br></br>
-            <img
-              src='https://assets.codepen.io/296057/fem-star.svg'
-              alt='Five star rating.'
-              className='star'
-            />
-            <img
-              src='https://assets.codepen.io/296057/fem-star.svg'
-              alt=''
-              className='star'
-            />
-            <img
-              src='https://assets.codepen.io/296057/fem-star.svg'
-              alt=''
-              className='star'
-            />
-            <img
-              src='https://assets.codepen.io/296057/fem-star.svg'
-              alt=''
-              className='star'
-            />
-            <img
-              src='https://assets.codepen.io/296057/fem-star.svg'
-              alt=''
-              className='star'
-            />
-          </blockquote>
-          <figcaption>
-            <cite>The Electric Vibe</cite>
-          </figcaption>
-        </figure>
-        <div className='featured-img'>
           <img
-            src='https://assets.codepen.io/296057/fem-blue-arms.jpg'
-            alt='Blue smoky stage and crowd with person raising arms.'
+            src='https://assets.codepen.io/296057/fem-star.svg'
+            alt=''
+            className='star'
           />
-        </div>
-        <figure>
-          <blockquote>
-            I can't stop listening<br></br>
-            <img
-              src='https://assets.codepen.io/296057/fem-star.svg'
-              alt='Four star rating.'
-              className='star'
-            />
-            <img
-              src='https://assets.codepen.io/296057/fem-star.svg'
-              alt=''
-              className='star'
-            />
-            <img
-              src='https://assets.codepen.io/296057/fem-star.svg'
-              alt=''
-              className='star'
-            />
-            <img
-              src='https://assets.codepen.io/296057/fem-star.svg'
-              alt=''
-              className='star'
-            />
-          </blockquote>
-          <figcaption>
-            <cite>Draft Media</cite>
-          </figcaption>
-        </figure>
-      </aside>
-    </div>
+          <img
+            src='https://assets.codepen.io/296057/fem-star.svg'
+            alt=''
+            className='star'
+          />
+        </blockquote>
+        <figcaption>
+          <cite>The Staple</cite>
+        </figcaption>
+      </figure>
+      <div className='featured-img'>
+        {' '}
+        <img
+          src='https://assets.codepen.io/296057/fem-purple-arms.jpg'
+          alt='Purple stage and excited crowd.'
+        />
+      </div>
+      <figure>
+        <blockquote>
+          Best. Album. Ever.<br></br>
+          <img
+            src='https://assets.codepen.io/296057/fem-star.svg'
+            alt='Four star rating.'
+            className='star'
+          />
+          <img
+            src='https://assets.codepen.io/296057/fem-star.svg'
+            alt=''
+            className='star'
+          />
+          <img
+            src='https://assets.codepen.io/296057/fem-star.svg'
+            alt=''
+            className='star'
+          />
+          <img
+            src='https://assets.codepen.io/296057/fem-star.svg'
+            alt=''
+            className='star'
+          />
+        </blockquote>
+        <figcaption>
+          <cite>Gold Spin</cite>
+        </figcaption>
+      </figure>
+      <div className='featured-img'>
+        {' '}
+        <img
+          src='https://assets.codepen.io/296057/fem-fireworks.jpg'
+          alt='Red stage with fireworks and excited crowd.'
+        />
+      </div>
+      <figure>
+        <blockquote>
+          These guys know how to party<br></br>
+          <img
+            src='https://assets.codepen.io/296057/fem-star.svg'
+            alt='Five star rating.'
+            className='star'
+          />
+          <img
+            src='https://assets.codepen.io/296057/fem-star.svg'
+            alt=''
+            className='star'
+          />
+          <img
+            src='https://assets.codepen.io/296057/fem-star.svg'
+            alt=''
+            className='star'
+          />
+          <img
+            src='https://assets.codepen.io/296057/fem-star.svg'
+            alt=''
+            className='star'
+          />
+          <img
+            src='https://assets.codepen.io/296057/fem-star.svg'
+            alt=''
+            className='star'
+          />
+        </blockquote>
+        <figcaption>
+          <cite>The Electric Vibe</cite>
+        </figcaption>
+      </figure>
+      <div className='featured-img'>
+        <img
+          src='https://assets.codepen.io/296057/fem-blue-arms.jpg'
+          alt='Blue smoky stage and crowd with person raising arms.'
+        />
+      </div>
+      <figure>
+        <blockquote>
+          I can't stop listening<br></br>
+          <img
+            src='https://assets.codepen.io/296057/fem-star.svg'
+            alt='Four star rating.'
+            className='star'
+          />
+          <img
+            src='https://assets.codepen.io/296057/fem-star.svg'
+            alt=''
+            className='star'
+          />
+          <img
+            src='https://assets.codepen.io/296057/fem-star.svg'
+            alt=''
+            className='star'
+          />
+          <img
+            src='https://assets.codepen.io/296057/fem-star.svg'
+            alt=''
+            className='star'
+          />
+        </blockquote>
+        <figcaption>
+          <cite>Draft Media</cite>
+        </figcaption>
+      </figure>
+    </aside>
   );
 };
 
